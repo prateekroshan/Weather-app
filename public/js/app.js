85470e6bcc557e796ab67445768252e0b255e36b
prateekroshan <prateekroshan6@gmail.com>
2021-06-03 13:15:28 +0530

    Unnecesseary console.log removed

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("hello.client side javascript running!!!");
-
 const weatheForm = document.querySelector("form");
 const searchForm = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
